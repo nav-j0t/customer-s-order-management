8addce570bcfbe3d629f844070f7cad27a12b9ee
NAVJOT SINGH <56749821+nav-j0t@users.noreply.github.com>
2021-12-21 14:52:39 -0800

    Update queries.sql
    /*  query used to replace customerID with customer's name  using JOIN */

--- queries.sql
+++ queries.sql
@@ -6,3 +6,11 @@ FROM Customers;
 /*  we will be checking about the orders whom they are ordered by and which payment id was used  and what was the order date */
 SELECT orderID "ORDER ID", customerID "Customer ID" , order_date "ORDER DATE" , status "STATUS OF ORDER" , paymentID "PAYMENT ID USED"
 FROM Orders;
+
+
+/*  query used to replace customerID with customer's name  using JOIN */
+SELECT o.orderID "ORDER ID", c.name "CUSTOMER NAME" , o.order_date "ORDER DATE" , o.status "STATUS OF ORDER" , o.paymentID "PAYMENT ID USED"
+FROM Orders o
+JOIN Customers c ON c.customerID = o.customerID;
+
+
