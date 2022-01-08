3448040f958a83119fe225ba2365498ea779dfc8
NAVJOT SINGH <56749821+nav-j0t@users.noreply.github.com>
2022-01-07 22:19:25 -0800

    Update script.sql

--- script.sql
+++ script.sql
@@ -5,7 +5,6 @@ CREATE TABLE Customers (
   phone varchar(100) default NULL,
   email varchar(255) default NULL,
   age int, 
-  
   address varchar(255) default NULL,
   PRIMARY KEY (customerID));
 
